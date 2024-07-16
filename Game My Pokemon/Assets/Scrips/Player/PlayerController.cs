7bbe712e4fa46176e87e7051d694792e8bd07abf
TrungKienLyf <coichetsdbg@gmail.com>
2024-07-17 05:13:16 +0700

    Movement player

--- Game My Pokemon/Assets/Scrips/Player/PlayerController.cs
+++ Game My Pokemon/Assets/Scrips/Player/PlayerController.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float moveSpeed;
+
+    private bool isMoving;
+    private Vector2 input;   
+
+    private void Update()
     {
-        
+        if (!isMoving)
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+            if (input.x != 0) input.y = 0;
+
+            if (input != Vector2.zero)
+            {
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+
+                StartCoroutine(Move(targetPos));
+            }
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Move(Vector3 targetPos)
     {
-        
+        isMoving = true;
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon) 
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+
+        isMoving = false;
     }
 }
